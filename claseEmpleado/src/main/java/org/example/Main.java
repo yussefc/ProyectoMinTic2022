e2b0e233f0bcb19d0f3a8a8c0eb3d24acbccc230
weysierrao <111469311+weysierrao@users.noreply.github.com>
2022-08-29 13:38:23 -0500

    se agrega clase empleado

--- claseEmpleado/src/main/java/org/example/Main.java
+++ claseEmpleado/src/main/java/org/example/Main.java
@@ -1,15 +1,51 @@
-package org.example;
+package udea.edu.com.empleado.empleado;
 
-public class Empleado {
+public class employed {
+    private String name;
+    private String email;
+    private empresa empresaQuePertence;
 
-     String name;
-     String mail;
-     String empresaQuePertenece;
-     String rolEmpleado;
+    public employed(String name, String email, String empresaQuePertence, String rolEmpleado) {
+        this.name = name;
+        this.email = email;
+        this.empresaQuePertence = empresaQuePertence;
+        this.rolEmpleado = rolEmpleado;
+    }
 
-     public static void main(String [] args){
-         Empleado empleado1 = new Empleado();
+    private String rolEmpleado;
 
 
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmpresaQuePertence() {
+        return empresaQuePertence;
+    }
+
+    public void setEmpresaQuePertence(String empresaQuePertence) {
+        this.empresaQuePertence = empresaQuePertence;
+    }
+
+    public String getRolEmpleado() {
+        return rolEmpleado;
+    }
+
+    public void setRolEmpleado(String rolEmpleado) {
+        this.rolEmpleado = rolEmpleado;
     }
 }
+
